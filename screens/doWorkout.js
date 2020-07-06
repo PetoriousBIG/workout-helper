bdcba673e17ad45b8a554db858b5b746f1fcea5d
unknown <peter.m.martin@maine.edu>
2020-07-06 18:00:10 -0400

    do workout page set up, started on modal

--- screens/doWorkout.js
+++ screens/doWorkout.js
@@ -1,30 +1,56 @@
-import React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList, Modal, TouchableOpacity } from 'react-native';
+import Set from '../model/set.js';
 
-//a tab for letting the user make customization and contains app info
+//screen for user to store results of in progress workout instance
 export default function DoWorkoutScreen({route, navigation}) {
+  const [modalIsEditing, setModalIsEditing] = useState(-1)
+  const [isModalVisible, setIsModalVisable] = useState(false)
+  
   const wo = route.params.item.exercises
   var sets = []
   var i
   for(i = 0; i < wo.length; i++){
     const nextExercise = wo[i]
     const numSets = nextExercise.sets
+    const goalReps = nextExercise.reps
+    var thisExerciseSets = []
     var j 
     for(j = 0; j < numSets; j++){
-        console.log(nextExercise.name + ' ' + (j+1))
+        thisExerciseSets.push(new Set(nextExercise.name, goalReps, 0, '', false ))
     }
+    sets = sets.concat(thisExerciseSets)
   }
-  
-  
+
   return (
-    <View style = {styles.topView}>
+    <View>
+
+      <Modal visible = {isModalVisible} presentationStyle= {'formSheet'} animationType={'fade'}>
+        <TouchableOpacity onPress={() => setIsModalVisable(false)}>
+          <Text>Close</Text>
+        </TouchableOpacity>
+      </Modal>
+
+      <FlatList data={sets}
+                keyExtractor={(item, index) => item.key}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => setIsModalVisable(true)}>
+                        <View style = {styles.flatList}>
+                            <Text style = {styles.text}>{item.exercise}: {item.completedReps}/{item.goalReps} @ {item.weight}</Text>
+                        </View>
+                    </TouchableOpacity>
+                )}>
+        
+      </FlatList>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  topView: {
-    flex: 1,
-    justifyContent: 'center'
+  flatList: {
+      padding: 15
   },
+  text: {
+      fontSize: 25
+  }
 })
