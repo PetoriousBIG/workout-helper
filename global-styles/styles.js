0eca83fadc44d07a6cd7e7d4ac92806afc967949
PetoriousBIG <peter.m.martin@maine.edu>
2020-08-03 18:29:25 -0400

    implementing global styles, refactoing

--- global-styles/styles.js
+++ global-styles/styles.js
@@ -1,10 +1,39 @@
 import { StyleSheet } from 'react-native'
 
-export default globalStyles = StyleSheet.create({
-    flatlist: {
-      padding: 15,
-      borderWidth: .5,
+const globalStyles = StyleSheet.create({
+    topContainer: {
+      flex: 1,
       flexDirection: "row",
-      justifyContent: "space-between"
+      justifyContent: "flex-start",
+    },
+    fab: {
+      position: 'absolute',
+      margin: 32,
+      right: 0,
+      bottom: 0,
+      borderColor: 'black',
+      borderWidth: 1 },
+
+    row: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: "center",
+      marginVertical: 15,
+    },
+    imageButtonRow: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      marginRight: "2.5%"
+    },
+    imageButton: {
+        width: 36,
+        height: 36,
+        marginHorizontal: "1%"
+    },
+    flatlistHeader: {
+      fontSize: 28,
+      marginLeft: '2.5%'
     }
 })
+
+export default globalStyles
