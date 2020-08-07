46172d5351ad654cb353ec82f4aa266a593c9af9
PetoriousBIG <peter.m.martin@maine.edu>
2020-08-06 20:25:54 -0400

    refactored input modal

--- src/screens/modal.js
+++ src/screens/modal.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Text, View, TouchableOpacity, Modal, TextInput} from 'react-native';
 import globalStyles from '../global-styles/styles'
 
+//the modal for updating the completed reps and weight for
+//exercises on the do-workout screen
 
 export default function InputModal(props){
     return (
@@ -17,11 +19,17 @@ export default function InputModal(props){
                   
                   <TextInput style={globalStyles.numberInput} maxLength={4} placeholder='Reps' keyboardType={'numeric'} value={props.viewReps}
                    onChangeText={(text) => props.updateReps(text)} onEndEditing={(event) => {
-                        const val = props.cleanNum(event)
-                        props.updateReps(val)}}/>
+
+                    //clean input and update rep for exercise at index given by flag
+                    //on do-workout screen
+                    const val = props.cleanNum(event)
+                    props.updateReps(val)}}/>
                   
                   <TextInput style={globalStyles.numberInput} maxLength={4} placeholder="Weight" keyboardType={'numeric'} value={props.viewWeight}
                    onChangeText={(text) => props.updateMW(text)} onEndEditing={(event) => {
+
+                    //clean input and update weight for exercise at index given by flag
+                    //on do-workout screen
                     const val = props.cleanNum(event)
                     props.updateMW(val)}}/>
                 
@@ -30,11 +38,13 @@ export default function InputModal(props){
                 <View style={globalStyles.row}>
 
                   <TouchableOpacity onPress={() => {
-                       props.saveResults(props.viewReps, props.viewWeight)     
-                       props.dismiss()}}>
+                      props.saveResults(props.viewReps, props.viewWeight)     
+                      props.dismiss()}}>
+                    
                     <View>
                       <Text>Save</Text>
                     </View>
+                  
                   </TouchableOpacity>
 
                   <TouchableOpacity onPress={() => {props.dismiss()}}>
